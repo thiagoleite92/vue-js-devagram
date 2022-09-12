4f7775c2446c3f9145ae44003c3ad3798e505baf
Thiago Leite <leite.tjs@gmail.com>
2022-09-12 20:19:52 -0300

    Aula 03 pt 2

---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import LoginView from '@/views/LoginView.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: LoginView,
+    },
+  ],
+});
+
+export default router;
---
+++ src/views/LoginView.vue
@@ -0,0 +1,8 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({});
+</script>
+
+<template>
+  <h1>Bem vindo devaggram</h1>
+</template>
