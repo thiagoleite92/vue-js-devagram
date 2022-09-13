00aa3583afb54cc75c3e417b1f3fe59227af1350
Thiago Leite <leite.tjs@gmail.com>
2022-09-12 22:05:38 -0300

    aula 3 pt 4 e 5 - estilização

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -24,23 +24,25 @@ export default defineComponent({
 </script>
 
 <template>
-  <div>
-    <img src="../assets/imagens/logo.svg" alt="Logo Devagram" />
+  <div class="container-publico">
+    <img src="../assets/imagens/logo.svg" alt="Logo Devagram" class="logo" />
     <form action="">
-      <p v-if="erro">{{ erro }}</p>
-      <div>
+      <p v-if="erro" class="error">{{ erro }}</p>
+      <div class="input">
         <img src="../assets/imagens/envelope.svg" alt="Insira Login" />
-        <input type="text" v-model="login" />
+        <input type="text" placeholder="Email" v-model="login" />
       </div>
-      <div>
+      <div class="input">
         <img src="../assets/imagens/key.svg" alt="Informe a Senha" />
-        <input type="password" v-model="senha" />
+        <input type="password" placeholder="Senha" v-model="senha" />
       </div>
       <button @click.enter.prevent="efetuarLogin()">Login</button>
-      <div>
+      <div class="link">
         <p>Não possui uma conta?</p>
         <a href="">Faça seu cadastro agora!</a>
       </div>
     </form>
   </div>
 </template>
+
+<style lang="scss" src="@/assets/styles/paginaPublica.scss" />
