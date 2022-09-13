3c33f3da2ee3dd5c9bac9551a0c0b8d6e77ce3c7
Thiago Leite <leite.tjs@gmail.com>
2022-09-12 22:49:42 -0300

    aula 3 pt 6 - input publico customizado

---
+++ src/components/InputPublico.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  props: {
+    icone: String,
+    alt: String,
+    tipo: String,
+    placeholder: String,
+    modelValue: String,
+  },
+  computed: {
+    model: {
+      get() {
+        return this.modelValue;
+      },
+      set(v: any) {
+        this.$emit('setInput', v);
+      },
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="container-input-customizado">
+    <img :src="icone" :alt="alt" />
+    <input :type="tipo" :placeholder="placeholder" v-model="model" />
+  </div>
+</template>
+
+<style lang="scss" src="@/assets/styles/inputPublico.scss" />
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,17 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
+import InputPublico from '../components/InputPublico.vue';
+
+import iconeLogin from '../assets/imagens/envelope.svg';
+import iconeSenha from '../assets/imagens/key.svg';
+
 export default defineComponent({
+  setup() {
+    return {
+      iconeLogin,
+      iconeSenha,
+    };
+  },
   data() {
     return {
       login: '',
@@ -9,17 +20,22 @@ export default defineComponent({
       erro: '',
     };
   },
-
   methods: {
     efetuarLogin() {
       if (!this.login && !this.senha) {
         this.erro = 'Favor preencher o formulário';
         return;
       }
-
       alert('Login efetuado com sucesso!');
     },
+    setLogin(v: any) {
+      this.login = v;
+    },
+    setSenha(v: any) {
+      this.senha = v;
+    },
   },
+  components: { InputPublico },
 });
 </script>
 
@@ -28,14 +44,24 @@ export default defineComponent({
     <img src="../assets/imagens/logo.svg" alt="Logo Devagram" class="logo" />
     <form action="">
       <p v-if="erro" class="error">{{ erro }}</p>
-      <div class="input">
-        <img src="../assets/imagens/envelope.svg" alt="Insira Login" />
-        <input type="text" placeholder="Email" v-model="login" />
-      </div>
-      <div class="input">
-        <img src="../assets/imagens/key.svg" alt="Informe a Senha" />
-        <input type="password" placeholder="Senha" v-model="senha" />
-      </div>
+
+      <InputPublico
+        :icone="iconeLogin"
+        alt="Insira o Login"
+        placeholder="Email"
+        tipo="text"
+        :modelValue="login"
+        @setInput="setLogin"
+      />
+
+      <InputPublico
+        :icone="iconeSenha"
+        alt="Insira a Senha"
+        placeholder="Senha"
+        tipo="password"
+        :modelValue="senha"
+        @setInput="setSenha"
+      />
       <button @click.enter.prevent="efetuarLogin()">Login</button>
       <div class="link">
         <p>Não possui uma conta?</p>
