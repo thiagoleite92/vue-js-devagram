0507665e923faf0da7049ecc29a311ac6cdcd218
Thiago Leite <leite.tjs@gmail.com>
2022-09-19 02:09:53 -0300

    Inicio header

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      resultado: [],
+    };
+  },
+});
+</script>
+
+<template>
+  <header class="container-header">
+    <div class="principal">
+      <img src="../assets/imagens/logoHorizontal.svg" alt="Logo Devagram" />
+      <div class="group">
+        <div class="pesquisa">
+          <img src="../assets/imagens/search.svg" class="icon" />
+          <input type="text" placeholder="Pesquisar" />
+        </div>
+
+        <!-- Navegacao -->
+      </div>
+    </div>
+
+    <div class="resultado" v-if="resultado.length > 0">
+      <!-- resultado busca -->
+    </div>
+  </header>
+</template>
+
+<style lang="scss" src="@/assets/styles/header.scss" />
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,10 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { LoginServices } from '../services/LoginServices';
-import router from '../router/';
+import Header from '../components/Header.vue';
 
-const loginService = new LoginServices();
-
-export default defineComponent({
-  setup() {
-    return {
-      loginService,
-    };
-  },
-  methods: {
-    sair() {
-      loginService.logout();
-      router.push({ name: 'login' });
-    },
-  },
-});
+export default defineComponent({ components: { Header } });
 </script>
 
 <template>
-  <div>
-    <button @click="sair">Sair</button>
-  </div>
+  <Header />
 </template>
