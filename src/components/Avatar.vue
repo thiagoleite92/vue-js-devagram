e080452d2e4244b32ad15e83a126f3a8792dff4f
Thiago Leite <leite.tjs@gmail.com>
2022-09-19 22:46:36 -0300

    estrutura postagem

---
+++ src/components/Avatar.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import avatarImg from '../assets/imagens/avatar.svg';
+
+export default defineComponent({
+  props: {
+    imagem: String,
+  },
+  computed: {
+    escolherAvatar(): string {
+      return this.imagem ? this.imagem : avatarImg;
+    },
+  },
+});
+</script>
+
+<template>
+  <img :src="escolherAvatar" alt="Foto do Usuário" class="avatar" />
+</template>
--- src/views/PostagemView.vue
+++ src/views/PostagemView.vue
@@ -1,15 +1,86 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Avatar from '../components/Avatar.vue';
+
+import likeRoxo from '../assets/imagens/likeRoxo.svg';
+import likeCinza from '../assets/imagens/likeCinza.svg';
+import comentarRoxo from '../assets/imagens/comentarRoxo.svg';
+import comentarCinza from '../assets/imagens/comentarCinza.svg';
 
 export default defineComponent({
+  setup() {
+    return {
+      likeRoxo: String,
+      curtirCinza: String,
+      comentarRoxo: String,
+      comentarCiza: String,
+      loggedUserId: localStorage.getItem('_id'),
+    };
+  },
   props: {
     post: null,
   },
+  methods: {
+    navegarParaPerfil() {},
+  },
+  computed: {
+    obterIconeCurtir() {
+      return this.post?.likes &&
+        this.post?.likes.findIndex((e: String) => e === this.loggedUserId) != -1
+        ? likeRoxo
+        : likeCinza;
+    },
+  },
+  components: { Avatar },
 });
 </script>
 
 <template>
-  <div class="container-postagem">{{ post?._id }}</div>
+  <div class="container-postagem">
+    <div @click="navegarParaPerfil()">
+      <section class="cabecalho">
+        <Avatar :imagem="post?.usuario?.avatar" />
+        <strong>{{ post?.usuario?.nome }}</strong>
+      </section>
+    </div>
+
+    <div class="foto">
+      <img :src="post?.foto" alt="Imagem da publicação" />
+    </div>
+
+    <div class="rodape">
+      <div class="acoes">
+        <img alt="Icone Curtir" :src="obterIconeCurtir" />
+        <img alt="Icone Comentario" src="../assets/imagens/comentarCinza.svg" />
+        <span class="curtidas"
+          >Curtido por
+          <strong
+            >{{ post?.likes?.length }}
+            pessoa
+            {{ post?.likes?.length > 1 ? 's' : '' }}
+          </strong>
+        </span>
+      </div>
+
+      <div class="descricao">
+        <strong>{{ post?.usuario?.nome }}</strong>
+        <p>
+          {{ post?.descricao }}
+        </p>
+      </div>
+
+      <div class="comentarios">
+        <div v-for="(comentario, index) in post?.comentarios" :key="index">
+          <strong>{{ comentario.nome }}</strong>
+          <p>{{ comentario.comentario }}</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-comentario">
+      <!-- implementar comentario -->
+    </div>
+  </div>
 </template>
 
 <!-- <style lang="scss" src="@/assets/styles/feed.scss" /> -->
